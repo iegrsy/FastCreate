529f88d83b02ead1bfe22a5a06343d020a3f63ca
Ibrahim Ethem Gursoy <ieg@bilkon-kontrol.com.tr>
2019-04-30 09:44:01 +0300

    Test streamer diagnostic

--- CSharpProject/Program.cs
+++ CSharpProject/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using Grpc.Core;
+using AlgorithmCommunication;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace CSharpProject
 {
@@ -6,7 +10,41 @@ namespace CSharpProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string _host = "192.170.0.28";
+            int _port = 2121;
+
+            Channel channel = new Channel(_host + ":" + _port, ChannelCredentials.Insecure);
+            DiagnosticService.DiagnosticServiceClient client = new DiagnosticService.DiagnosticServiceClient(channel);
+
+            new Task(async () => { await streamStatusAsync(client); }).Start();
+
+            System.Console.ReadKey();
+        }
+
+        static void getStatus(DiagnosticService.DiagnosticServiceClient client)
+        {
+            try
+            {
+                DiagnosticInfo info = client.GetDiagnosticStatus(new DiagnosticQ());
+                System.Console.WriteLine(info.ToString());
+            }
+            catch (Grpc.Core.RpcException e) { System.Console.WriteLine(e.Message); }
+        }
+
+        static async System.Threading.Tasks.Task streamStatusAsync(DiagnosticService.DiagnosticServiceClient client)
+        {
+            try
+            {
+                using (var call = client.GetDiagnosticStatusStream(new DiagnosticQ()))
+                {
+                    while (await call.ResponseStream.MoveNext())
+                    {
+                        DiagnosticInfo info = call.ResponseStream.Current;
+                        Console.WriteLine("Received: " + info.ToString());
+                    }
+                }
+            }
+            catch (Grpc.Core.RpcException e) { System.Console.WriteLine(e.Message); }
         }
     }
 }
